775ecf4a3c964fb87249ce7d975715f78b5a6cf1
Jo Wagner <jowagner@limago.de>
2022-08-16 11:50:34 +0200

    erste Lösung

--- Tag2_02Stapel/src/de/basf/collections/StapelImpl.java
+++ Tag2_02Stapel/src/de/basf/collections/StapelImpl.java
@@ -2,33 +2,41 @@ package de.basf.collections;
 
 public class StapelImpl implements Stapel {
 
+	private int [] data;
+	private int index;
+	
 	
 	public StapelImpl() {
-		
+		data = new int [10];
+		index = 0;
 	}
 	
 	@Override
 	public void push(int value) {
+		if(isFull()) 
+			return;
 		
+		data[index++] = value;
 
 	}
 
 	@Override
 	public int pop() {
+		if(isEmpty())
+			return 0;
 		
-		return 0;
+		return data[--index];
 	}
 
 	@Override
 	public boolean isEmpty() {
-		
-		return true;
+		return index <= 0;
 	}
 
 	@Override
 	public boolean isFull() {
 		
-		return true;
+		return index >= data.length;
 	}
 
 }
