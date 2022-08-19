be5c2f532540fede138bb550bcfe6f9fa8e58eb2
Jo Wagner <jowagner@limago.de>
2022-08-19 10:24:32 +0200

    Initial commit

---
+++ Tag5_02Collections/src/de/basf/Demo.java
@@ -0,0 +1,43 @@
+package de.basf;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+//		try (Scanner scanner = new Scanner(System.in)) {
+//			System.out.println("Bitte Name eingeben: ");
+//			String name = scanner.nextLine();
+//			System.err.println("Guten Tag " + name);
+//			
+//			Set<String> mySet = new TreeSet<>();
+//			
+//			
+//			
+//			mySet.add("Eins");
+//			mySet.add("Eins");
+//			mySet.add("Zwei");
+//			mySet.add("Drei");
+//			mySet.add("Vier");
+//			mySet.add("Fuenf");
+//			
+//			for (String item : mySet) {
+//				System.out.println(item);
+//			}
+//		
+//		}
+		
+		// Eingabeschleife
+			// liest woerter von der Konsole solange bis das Wort "Ende" eingegeben wird
+			// die Woerter werden in einer geeigneten Collection gespeichert
+			// Ende wird nicht gespeichert
+		
+		// Ausgabeschleife
+			// In gleicher Reihenfolge ausgeben in der sie erfasst wurden
+		
+	}
+
+}
